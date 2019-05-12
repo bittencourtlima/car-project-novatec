e8d936d979cda1d26ee4d0893e4534e2eef1816f
Anderson Bittencourt <andersonbittencourt@brq.com>
2019-05-12 19:59:43 -0300

    14.3 - Refatoração CarrosActivity para uso da Fragment

--- app/src/main/java/livroandroid/com/br/carros/activity/CarrosActivity.kt
+++ app/src/main/java/livroandroid/com/br/carros/activity/CarrosActivity.kt
@@ -10,12 +10,12 @@ import livroandroid.com.br.carros.domain.Carro
 import livroandroid.com.br.carros.domain.CarroService
 import livroandroid.com.br.carros.domain.TipoCarro
 import livroandroid.com.br.carros.extensions.setupToolbar
+import livroandroid.com.br.carros.fragments.CarrosFragment
 import org.jetbrains.anko.startActivity
 
 class CarrosActivity : BaseActivity() {
 
     private var tipo: TipoCarro = TipoCarro.Classicos
-    private var carros = listOf<Carro>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carros)
@@ -23,23 +23,14 @@ class CarrosActivity : BaseActivity() {
         val tipoCarro = getString(tipo.tipoCarro)
         setupToolbar(R.id.toolbar, tipoCarro, true)
 
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.itemAnimator = DefaultItemAnimator()
-        recyclerView.setHasFixedSize(true)
+        if(savedInstanceState == null){
+            val fragment = CarrosFragment()
+
+            fragment.arguments = intent.extras
+
+            supportFragmentManager.beginTransaction().add(R.id.container, fragment).commit()
+        }
 
     }
 
-    override fun onResume() {
-        super.onResume()
-        taskCarros()
-    }
-
-    private fun taskCarros() {
-        val carros = CarroService.getCarros(context, tipo)
-        recyclerView.adapter = CarroAdapter(carros) { onClickCarro(it) }
-    }
-
-    private fun onClickCarro(carro: Carro) {
-        startActivity<CarroActivity>("carro" to carro)
-    }
 }
--- app/src/main/java/livroandroid/com/br/carros/fragments/CarrosFragment.kt
+++ app/src/main/java/livroandroid/com/br/carros/fragments/CarrosFragment.kt
@@ -6,14 +6,13 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.activity_carros.*
+import kotlinx.android.synthetic.main.fragment_carros.*
 import livroandroid.com.br.carros.R
 import livroandroid.com.br.carros.activity.CarroActivity
 import livroandroid.com.br.carros.adapter.CarroAdapter
 import livroandroid.com.br.carros.domain.Carro
 import livroandroid.com.br.carros.domain.CarroService
 import livroandroid.com.br.carros.domain.TipoCarro
-import livroandroid.com.br.carros.fragments.BaseFragment
 import org.jetbrains.anko.startActivity
 
 class CarrosFragment : BaseFragment() {
