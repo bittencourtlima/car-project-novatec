c44776353af0d3b4bbcc9318e1e9bd477dbb7b35
Anderson Bittencourt <andersonbittencourt@brq.com>
2019-05-14 00:21:44 -0300

    15.4 - Collapsable

--- app/src/main/java/livroandroid/com/br/carros/activity/CarroActivity.kt
+++ app/src/main/java/livroandroid/com/br/carros/activity/CarroActivity.kt
@@ -3,6 +3,7 @@ package livroandroid.com.br.carros.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_carro.*
+import kotlinx.android.synthetic.main.include_activity_carro.*
 import livroandroid.com.br.carros.R
 import livroandroid.com.br.carros.domain.Carro
 import livroandroid.com.br.carros.extensions.loadImage
@@ -10,14 +11,15 @@ import livroandroid.com.br.carros.extensions.setupToolbar
 
 class CarroActivity : AppCompatActivity() {
 
-    val carro by lazy { intent.getSerializableExtra("carro") as Carro }
+    val carro by lazy {
+        intent.getParcelableExtra("carro") as Carro }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carro)
         setupToolbar(R.id.toolbar, carro.nome,true);
 
-        tDesc.text = carro?.desc
-        img.loadImage(carro?.urlFoto)
+        tDesc.text = carro.desc
+        appBarImg.loadImage(carro.urlFoto)
     }
 }
