932f8affb98387fb4fbc2c216d40ec66cdf357bf
Anderson Bittencourt <andersonbittencourt@brq.com>
2019-05-14 23:54:06 -0300

    16.2 - Leitura .xml na pasta raw

--- app/src/main/java/livroandroid/com/br/carros/domain/CarroService.kt
+++ app/src/main/java/livroandroid/com/br/carros/domain/CarroService.kt
@@ -1,24 +1,55 @@
 package livroandroid.com.br.carros.domain
 
 import android.content.Context
+import android.icu.lang.UCharacter.GraphemeClusterBreak.L
+import android.util.Log
+import livroandroid.com.br.carros.R
+import livroandroid.com.br.carros.extensions.getText
+import livroandroid.com.br.carros.extensions.getXml
 
 object CarroService {
 
-    fun getCarros(context: Context, tipo: TipoCarro): List<Carro>{
-        val tipoString = context.getString(tipo.tipoCarro)
-        val carros = mutableListOf<Carro>()
+    private const val TAG = "livro"
 
-        for(i in 1..20){
+    fun getCarros(context: Context, tipo: TipoCarro): List<Carro>{
+        val raw = getArquivoRaw(tipo)
+
+        val resources = context.resources
+
+        val inputStream = resources.openRawResource(raw)
+        inputStream.bufferedReader().use {
+            val xml = it.readText()
+            val carros = parserXML(xml)
+            return carros
+        }
+    }
+
+    private fun getArquivoRaw(tipo: TipoCarro) = when(tipo){
+        TipoCarro.Classicos -> R.raw.carros_classicos
+        TipoCarro.Esportivos -> R.raw.carros_esportivos
+        else -> R.raw.carros_luxo
+    }
+
+    private fun parserXML(xmlString: String): List<Carro>{
+        val carros = mutableListOf<Carro>()
+        val xml = xmlString.getXml()
+
+        val nodeCarros = xml.getChildren("carro")
+
+        for(node in nodeCarros){
             val c = Carro()
 
-            c.nome = "Carro: $tipoString: $i"
-            c.desc = "Desc $i"
+            c.nome = node.getText("nome")
+            c.desc = node.getText("desc")
+            c.urlFoto = node.getText("url_foto")
+            Log.d("carro",c.urlFoto)
 
-            c.urlFoto = "http://www.livroandroid.com.br/livro/carros/esportivos/Ferrari_FF.png"
             carros.add(c)
         }
-
+        Log.d(TAG, "${carros.size} carros encontrados")
         return carros
     }
+
+
 
 }
